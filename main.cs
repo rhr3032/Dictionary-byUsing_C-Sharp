e771a976afb2ba995076d7980995e0e1cd0ce4d5
Raisul Hasan Rafi <raisulhasanrafi3032@gmail.com>
2021-10-25 17:20:56 -0700

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,6 @@
+Dictionary<string, int> student = new();
+student.Add("John",9863);
+student.Add("Albart",2548);
+student.Add("Devi",3524);
+
+Console.WriteLine("Albert inscription is: " + student["Albart"]);
